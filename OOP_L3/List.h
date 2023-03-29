8de37362092207d872b9c49795b5e23192139b63
Drag365 <ksilantev92@gmail.com>
2023-03-29 22:15:53 +0500

    Добавьте файлы проекта.

---
+++ OOP_L3/List.h
@@ -0,0 +1,71 @@
+template<class T> class List {
+private:
+    int n = 0;
+    T* mas;
+    T* new_a;
+
+    void increase_arr(T*& a, int k) {
+        int new_n = n + k;
+        new_a = new T[new_n];
+        memcpy(new_a, a, n * sizeof(T));
+        delete[] a;
+        a = new_a;
+        n = new_n;
+    };
+public:
+    List() {
+        mas = new T[n];
+    };
+
+    List(const List& List) {
+        n = List.n;
+        mas = new T[n];
+        for (int i = 0; i < n; ++i)
+            mas[i] = this->mas[i];
+    }
+    ~List() {
+        delete[] mas;
+    }
+
+    void push_back(T* x) {
+        increase_arr(mas, 1);
+        for (int i = n - 1; i > 0; i--)
+            mas[i] = mas[i - 1];
+        mas[0] = *x;
+    };
+
+    void push(T* x) {
+        increase_arr(mas, 1);
+        mas[n - 1] = *x;
+    };
+
+    void Del(int j) {
+        if (n == 0)
+            return;
+        for (int i = j; i < n; i++)
+            mas[i] = mas[i + 1];
+        increase_arr(mas, -1);
+    };
+
+    void Set(T* x, int j) {
+        if (n != 0)
+            mas[j] = *x;
+    };
+
+    void Add(T* x, int j) {
+        increase_arr(mas, 1);
+        for (int i = n - 1; i >= j; i--)
+            mas[i] = mas[i - 1];
+        mas[j] = *x;
+    };
+
+  
+
+
+    T get(int i) {
+        return mas[i];
+    }
+    int getSize() {
+        return n;
+    }
+};
---
+++ OOP_L3/OOP_L3.cpp
@@ -0,0 +1,91 @@
+﻿// OOP_L3.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+#include <iostream>
+#include "List.h"
+
+class Test {
+protected:
+    int value;
+public:
+    Test() {
+        value = 5;
+    }
+    void setValue(int value) {
+        this->value = value;
+    }
+    int getValue() {
+        return value;
+    }
+};
+
+
+
+int main()
+{
+    srand(time(NULL));
+    setlocale(LC_ALL, "RUS");
+    List<Test> list;
+
+    int itternum = 100;
+    int count = 0;
+    while (count < 3) {
+        long double time_spent = 0;
+        clock_t begin = clock();
+        for (int i = 0; i < itternum; i++) {
+            switch (rand() % 3) {
+                int r;
+            case 0:
+                if (list.getSize() == 0)
+                    r = 0;
+                else
+                    r = rand() % (list.getSize());
+                list.Del(r);
+                break;
+            case 1:
+                switch (rand() % 3) {
+                case 0:
+                    if (list.getSize() == 0)
+                        r = 0;
+                    else
+                        r = rand() % (list.getSize());
+                    list.Add(new Test(), r);
+                    break;
+                case 1:
+                    list.push(new Test());
+                    break;
+                case 2:
+                    list.push_back(new Test());
+                    break;
+                }
+                break;
+            case 2:
+                switch (rand() % 3) {
+                case 0:
+                    list.getSize();
+                    break;
+                case 1:
+                    if (list.getSize() == 0)
+                        r = 0;
+                    else
+                        r = rand() % (list.getSize());
+                    list.Set(new Test(), r);
+                    break;
+                case 2:
+                    if (list.getSize() == 0)
+                        r = 0;
+                    else
+                        r = rand() % (list.getSize());
+                    list.get(r).getValue();
+                    break;
+                }
+                break;
+            }
+
+        }
+        clock_t end = clock();
+        time_spent += (double)(end - begin) / CLOCKS_PER_SEC;
+        printf("Итераций: %d; Время: %lf seconds\n", itternum, time_spent);
+        count++;
+        itternum *= 10;
+    }
+}
